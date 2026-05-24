669866928db9188bde6168b7bda0cef446f1ac88
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-24 22:31:22 +0300

    added a skeletal implementation of `execution`

--- vm/stack_vm/execute.cpp
+++ vm/stack_vm/execute.cpp
@@ -1,5 +1,105 @@
 #include "../vm.hpp"
+#include <stdexcept>
+
 
 void vm::stack_vm::execute()
 {
+    if (this->type == types::INSTRUCTIONS)
+    {
+        switch (this->type)
+        {
+        case instructions::HALT: {
+            this->stop();
+            break;
+        }
+
+        case instructions::PUSH:{
+            break;
+        }
+        case instructions::POP:{
+            break;
+        }
+        case instructions::PUSH_BP:{
+            break;
+        }
+        case instructions::POP_BP:{
+            break;
+        }
+        case instructions::MOV_BP_SP:{
+            break;
+        }
+        case instructions::MOV_SP_BP:{
+            break;
+        }
+        case instructions::SWAP:{
+            break;
+        }
+        case instructions::CALL:{
+            break;
+        }
+        case instructions::RET:{
+            break;
+        }
+        case instructions::LOAD:{
+            break;
+        }
+        case instructions::STORE:{
+            break;
+        }
+        case instructions::WRITE:{
+            break;
+        }
+        case instructions::DP:{
+            break;
+        }
+        case instructions::ADD:{
+            break;
+        }
+        case instructions::SUBTRACT:{
+            break;
+        }
+        case instructions::MULTIPLY:{
+            break;
+        }
+        case instructions::DIVIDE:{
+            break;
+        }
+        case instructions::JMP:{
+            break;
+        }
+        case instructions::JZ:{
+            break;
+        }
+        case instructions::JNZ:{
+            break;
+        }
+        case instructions::CMP_EQ:{
+            break;
+        }
+        case instructions::CMP_NEQ:{
+            break;
+        }
+        case instructions::CMP_GT:{
+            break;
+        }
+        case instructions::CMP_LT:{
+            break;
+        }
+        case instructions::CMP_GT_EQ:{
+            break;
+        }
+        case instructions::CMP_LT_EQ:{
+            break;
+        }
+
+        default:{
+            throw std::runtime_error("Unknown operand. Please check on operand and if you intended for it to be. Add it to the switch case");
+            break;
+        }
+        }
+    }
+    else
+    {
+        return;
+    }
 }
