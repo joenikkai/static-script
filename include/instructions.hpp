9d233c7127cb32cf5466c38fee290bea3215ca2f
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-24 14:21:51 +0300

    decided to encode data and instruction in the same 64 bit / 8 byte word. `uint64_t`

--- include/instructions.hpp
+++ include/instructions.hpp
@@ -1,7 +1,69 @@
 #ifndef INSTRUCTIONS_HPP
 #define INSTRUCTIONS_HPP
-enum instructions
+#include <cstdint>
+
+/**
+ * we are going to encode data type in the first 4 bits therefore the maximum value allowed is 1.152921505×10¹⁸
+ * 0 Instruction
+ * 1 address/pointer
+ * 2 int
+ * 3 float
+ * 4 bool
+ * 5 char
+ *
+ * You might ask the last (16-6)=10 values for. They are for future adjustments
+ */
+
+enum types : uint64_t
 {
-    INSTRUCTIONS_COUNT
+    INSTRUCTIONS = 0x0,
+    ADDRESS,
+    INTEGER,
+    FLOAT,
+    BOOLEAN,
+    CHARACTER,
+    TYPE_COUNT
 };
-#endif // INSTRUCTIONS_HPP
+
+enum instructions : uint64_t
+{
+    HALT = 0x0,
+    PUSH, /* Push to the stack */
+    POP,  /* Pop from the stack */
+
+    PUSH_BP, /* Push current base pointer */
+    POP_BP, /* Revert to previous base pointer */
+
+    MOV_BP_SP, /* Move base pointer to stack pointer */
+    MOV_SP_BP, /* Move stack pointer to base pointer */
+
+    SWAP, 
+    CALL, /* used to call a function */
+    RET,  /* return a value from a function call */
+
+    LOAD,  /* Read from a variable */
+    STORE, /* Store to a variable  */
+    WRITE, /* Print */
+    DP,    /* Duplicate */
+
+    ADD,
+    SUBTRACT,
+    MULTIPLY,
+    DIVIDE,
+
+    JMP, /* Jump */
+    JZ,  /* Jump if zero */
+    JNZ, /* Jump if not zero */
+
+    CMP_EQ,  /* compare if equal */
+    CMP_NEQ, /* compare not equal to */
+
+    CMP_GT, /* compare if greater than */
+    CMP_LT, /* compareif less than */
+
+    CMP_GT_EQ, /* compare greater than or equals to  */
+    CMP_LT_EQ, /* compare less than or equal to  */
+
+    INSTRUCTIONS_COUNT /* Number of instructions */
+};
+#endif /* INSTRUCTIONS_HPP */
