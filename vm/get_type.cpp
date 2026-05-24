4e31d446b346eb02381e6a3f77e2c6936a7fb48c
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-24 22:47:30 +0300

    removed the debug print

--- vm/get_type.cpp
+++ vm/get_type.cpp
@@ -7,7 +7,5 @@
 uint8_t vm::vm::get_type(uint64_t word)
 {
     // we shift the first 56 bits so as to bring forward the last 8 bits then mask the last 8 bits with 256
-    uint8_t type = (uint8_t)((word >> 56) & 0xff);
-    std::cout << type << '\n';
-    return type;
+    return (uint8_t)((word >> 56) & 0xff);
 }
