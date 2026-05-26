8776c48e1fac3d3f05a0caf4056b5c098a576f69
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-26 21:17:47 +0300

    changed them struct

--- vm/stack_vm/decode.cpp
+++ vm/stack_vm/decode.cpp
@@ -3,6 +3,6 @@
 void vm::stack_vm::decode()
 {
     uint64_t address = this->fetch();
-    this->type = this->get_type(this->get_word(address));
-    this->data = this->get_data(this->get_word(address));
+    this->opcode.type = this->get_type(this->get_word(address));
+    this->opcode.data = this->get_data(this->get_word(address));
 }
