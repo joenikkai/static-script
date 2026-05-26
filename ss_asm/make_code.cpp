db9404767960fb7d93ef0d78d7a599405f0675c1
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-26 20:48:45 +0300

    fixed the `DATA_REGION` to intended `DATA_REGION_BITS`

--- ss_asm/make_code.cpp
+++ ss_asm/make_code.cpp
@@ -5,5 +5,5 @@ uint64_t ss_asm::ss_asm::make_code(enum types t, uint64_t d)
     // uint64_t c = t << DATA_REGION;
     // c += d;
     // return c;
-    return (t << DATA_REGION) + d;
+    return (t << DATA_REGION_BITS) + d;
 }
