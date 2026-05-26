ec6ee8fdd205f231e3e497f55c0ef1c6f0d5f9df
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-26 20:05:39 +0300

    fixed the `decode` functionality

--- vm/stack_vm/decode.cpp
+++ vm/stack_vm/decode.cpp
@@ -2,6 +2,7 @@
 
 void vm::stack_vm::decode()
 {
-    this->type = this->get_type(this->get_word(this->pc));
-    this->data = this->get_data(this->get_word(this->pc));
+    uint64_t address = this->fetch();
+    this->type = this->get_type(this->get_word(address));
+    this->data = this->get_data(this->get_word(address));
 }
