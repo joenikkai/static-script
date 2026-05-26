ad3c83485caba4ddd944ff85b8d0ef00d475893d
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-26 20:58:31 +0300

    using the assembler and the stack virtual machine

--- main.cpp
+++ main.cpp
@@ -1,23 +1,33 @@
 #include <iostream>
 #include "vm/vm.hpp"
-
+#include "ss_asm/ss_asm.hpp"
 
 #define DEBUG
 #undef DEBUG
 
 int main(int argc, char** argv)
 {
-    std::vector<uint64_t> program{
-        ss_asm::ss_asm::make_code(types::INSTRUCTIONS, instructions::PUSH),
-        ss_asm::ss_asm::make_code(types::INTEGER, 256),
-        ss_asm::ss_asm::make_code(types::INSTRUCTIONS, instructions::PUSH),
-        ss_asm::ss_asm::make_code(types::CHARACTER, '\n'),
-        ss_asm::ss_asm::make_code(types::INSTRUCTIONS, instructions::WRITE),
-        ss_asm::ss_asm::make_code(types::INSTRUCTIONS, instructions::WRITE),
-        ss_asm::ss_asm::make_code(types::INSTRUCTIONS, instructions::HALT)};
+    ss_asm::ss_asm assembler;
     std::cout << ("This will be out main entry point\n");
-    std::vector<uint64_t> program{0};
-    vm::stack_vm s_vm(program);
-    s_vm.get_data(1000000000);
+    std::vector<uint64_t> program{
+        assembler.make_code(types::INSTRUCTIONS, instructions::PUSH),
+        assembler.make_code(types::INTEGER, 256),
+        assembler.make_code(types::INSTRUCTIONS, instructions::PUSH),
+        assembler.make_code(types::CHARACTER, '\n'),
+        assembler.make_code(types::INSTRUCTIONS, instructions::WRITE),
+        assembler.make_code(types::INSTRUCTIONS, instructions::WRITE),
+        assembler.make_code(types::INSTRUCTIONS, instructions::HALT)
+    };
+    vm::stack_vm ss_vm(program);
+#ifdef DEBUG
+    for (uint64_t& code:program)
+    {
+        std::cout << "op code:" << 
+        "\n\t| type: " << (uint16_t) ss_vm.get_type(code) << " | "
+        "\n\t| data: " << ss_vm.get_data(code) << " | "
+        "\n";
+    }
+#endif // DEBUG
+    ss_vm.run();
     return 0;
 }
