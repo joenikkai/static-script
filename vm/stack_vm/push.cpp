b87bcbe5a16df6560b236d6853b023460f71104d
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-26 21:30:30 +0300

    updated implementation for push

--- vm/stack_vm/push.cpp
+++ vm/stack_vm/push.cpp
@@ -2,5 +2,11 @@
 
 void vm::stack_vm::push()
 {
-    this->stack.push_back(this->get_word(pc));
+    this->fetch();
+    uint64_t data = this->get_word(pc);
+    struct op_code_type1 oc{
+        .type = get_type(data),
+        .data = get_data(data)
+    };
+    this->stack.push_back(oc);
 }
