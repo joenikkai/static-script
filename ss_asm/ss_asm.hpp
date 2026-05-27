4b118a615feb6352ad6ef8dd15f1cfcd5b675463
Joseph Wangai Mwaniki <joewamwaniki@gmail.com>
2026-05-27 03:08:29 +0300

    added token types

--- ss_asm/ss_asm.hpp
+++ ss_asm/ss_asm.hpp
@@ -4,17 +4,42 @@
 
 #include "../include/instructions.hpp"
 #include <cstdint>
+#include <vector>
+#include <string>
+
+/**
+ * This will search for global start
+ */
 
 namespace ss_asm 
 {
+    enum token_type
+    {
+        INTEGER,
+        FLOAT,
+        BOOLEAN,
+        CHARACTER,
+        STRING,
+        END_OF_FILE,
+        TOKEN_TYPES_COUNT
+    };
+
+    struct token
+    {
+        std::string value;
+        token_type t;
+    };
     class ss_asm
     {
-    private:
-        /* data */
-    public:
-        ss_asm(/* args */);
-        uint64_t make_code(enum types t,uint64_t d);
-        ~ss_asm();
+        private:
+            std::vector<std::string> filenames;
+
+        public:
+            ss_asm(/* args */);
+            uint64_t make_code(enum types t,uint64_t d);
+            virtual std::vector<struct token> lex();
+            virtual std::vector<std::string> parse(std::vector<struct token> nodes);
+            ~ss_asm();
     };
     
 
